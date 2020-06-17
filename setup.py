2bf072d6251c66595f12fa3249f7aa6a5a7b235e
pilotchute <akootz@ucar.edu>
2020-06-17 17:20:12 -0600

    added tests to geocat namespace as geocat.ntest
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
-#import distutils.sysconfig
-from Cython.Build import cythonize
-import numpy
 import os
 import sys
+
+import numpy
+# import distutils.sysconfig
+from Cython.Build import cythonize
 
 with open("src/geocat/ncomp/version.py") as f:
     exec(f.read())
@@ -21,22 +22,22 @@ include_dirs = [os.path.join(PREFIX, 'include'), numpy.get_include()]
 
 extensions = [
     Extension("geocat.ncomp._ncomp", ["src/geocat/ncomp/_ncomp.pyx"],
-        include_dirs=include_dirs,
-        libraries=["ncomp"],
-),
+              include_dirs=include_dirs,
+              libraries=["ncomp"],
+              ),
 ]
 setup(
     name="geocat.ncomp",
     ext_modules=cythonize(extensions,
                           # help cythonize find my own .pxd files
                           include_path=[os.path.join(SRC_DIR, "src/geocat/ncomp/_ncomp")]),
-    package_dir={'': 'src', 'geocat': 'src/geocat', 'geocat.ncomp': 'src/geocat/ncomp'},
+    package_dir={'': 'src', 'geocat': 'src/geocat', 'geocat.ncomp': 'src/geocat/ncomp', 'geocat.ntest': 'test'},
     package_data={'geocat': ['__init__.pxd', 'ncomp/*.pxd']},
     namespace_packages=['geocat'],
-    packages=["geocat", "geocat.ncomp"],
-    version=__version__, 
+    packages=['geocat', 'geocat.ncomp', 'geocat.ntest'],
+    version=__version__,
     install_requires=[
-        'numpy', 
-        'cython', 
+        'numpy',
+        'cython',
     ]
 )
