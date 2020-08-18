1c808ff47f91772ba4ee89420147e296bc03d564
erogluorhan <erogluorhan@gmail.com>
2020-08-18 11:14:21 -0600

    Added a few comments into build-libncomp.sh

--- .circleci/build-libncomp.sh
+++ .circleci/build-libncomp.sh
@@ -3,6 +3,7 @@
 set -e
 set -eo pipefail
 
+# Install conda environment
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda config --add channels conda-forge
 conda list -f python -e >> /usr/local/conda-meta/pinned
@@ -13,6 +14,8 @@ git checkout ${CIRCLE_BRANCH} || echo "No ${CIRCLE_BRANCH} on libncomp"
 conda env create -f .circleci/environment-dev-$(uname).yml --name ${LIBNCOMP_ENV_NAME} --quiet
 conda env list
 source activate ${LIBNCOMP_ENV_NAME}
+
+# Build libNComp
 autoreconf --install
 ./configure --prefix=${CONDA_PREFIX}
 make install
